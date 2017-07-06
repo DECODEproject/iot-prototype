6961366670cdb310edd6ef02fe7d4819ffe6c923
Mark deVilliers <markdevilliers@gmail.com>
2017-07-06 12:57:21 +0100

    validate json objects

--- metadata/services/catalog.go
+++ metadata/services/catalog.go
@@ -3,6 +3,8 @@ package services
 import (
 	"net/http"
 
+	validator "gopkg.in/validator.v2"
+
 	restful "github.com/emicklei/go-restful"
 	restfulspec "github.com/emicklei/go-restful-openapi"
 	uuid "github.com/satori/go.uuid"
@@ -14,24 +16,24 @@ type catalogResource struct {
 }
 
 type CatalogRequest struct {
-	LocationUID string   `json:"location-uid" description:"a valid location of a node registered previously via /announce"`
-	Key         string   `json:"key" description:"path of the data item"`
-	Tags        []string `json:"tags" description:"a collection of tags probably belonging to an ontology"`
+	LocationUID string   `json:"location-uid" description:"a valid location of a node registered previously via /announce" validate:"nonzero"`
+	Key         string   `json:"key" description:"path of the data item" validate:"nonzero"`
+	Tags        []string `json:"tags" description:"a collection of tags probably belonging to an ontology" validate:"nonzero"`
 }
 
 type Item struct {
 	CatalogRequest
-	UID string `json:"uid" description:"unique identifier for a metadata item"`
+	UID string `json:"uid" description:"unique identifier for a metadata item" validate:"nonzero"`
 }
 
 type LocationRequest struct {
-	IPAddress string `json:"ip-address" description:"public IP address of the node"`
-	Port      int    `json:"port" description:"public port of the node"`
+	IPAddress string `json:"ip-address" description:"public IP address of the node" validate:"nonzero"`
+	Port      int    `json:"port" description:"public port of the node" validate:"nonzero"`
 }
 
 type Location struct {
 	LocationRequest
-	UID string `json:"uid" description:"unique identifier for a node"`
+	UID string `json:"uid" description:"unique identifier for a node" validate:"nonzero"`
 }
 
 type ItemWithLocation struct {
@@ -49,10 +51,14 @@ func NewCatalogService() catalogResource {
 func (e catalogResource) registerLocation(request *restful.Request, response *restful.Response) {
 
 	req := LocationRequest{}
-	err := request.ReadEntity(&req)
 
-	if err != nil {
+	if err := request.ReadEntity(&req); err != nil {
 		response.WriteErrorString(http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	if errs := validator.Validate(req); errs != nil {
+		response.WriteErrorString(http.StatusBadRequest, errs.Error())
 		return
 	}
 
@@ -75,6 +81,11 @@ func (e catalogResource) moveLocation(request *restful.Request, response *restfu
 
 	if err != nil {
 		response.WriteErrorString(http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	if errs := validator.Validate(req); errs != nil {
+		response.WriteErrorString(http.StatusBadRequest, errs.Error())
 		return
 	}
 
@@ -101,6 +112,11 @@ func (e catalogResource) catalogItem(request *restful.Request, response *restful
 
 	if err != nil {
 		response.WriteErrorString(http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	if errs := validator.Validate(req); errs != nil {
+		response.WriteErrorString(http.StatusBadRequest, errs.Error())
 		return
 	}
 
@@ -161,6 +177,7 @@ func (e catalogResource) WebService() *restful.WebService {
 		Metadata(restfulspec.KeyOpenAPITags, tags).
 		Reads(LocationRequest{}).
 		Returns(http.StatusOK, "OK", Location{}).
+		Returns(http.StatusBadRequest, "error validating request", nil).
 		Returns(http.StatusInternalServerError, "something went wrong", nil))
 
 	// move a location
@@ -170,6 +187,7 @@ func (e catalogResource) WebService() *restful.WebService {
 		Metadata(restfulspec.KeyOpenAPITags, tags).
 		Reads(LocationRequest{}).
 		Returns(http.StatusOK, "OK", Location{}).
+		Returns(http.StatusBadRequest, "error validating request", nil).
 		Returns(http.StatusNotFound, "Not found", nil).
 		Returns(http.StatusInternalServerError, "something went wrong", nil))
 
@@ -179,6 +197,7 @@ func (e catalogResource) WebService() *restful.WebService {
 		Metadata(restfulspec.KeyOpenAPITags, tags).
 		Reads(Item{}).
 		Returns(http.StatusOK, "OK", Item{}).
+		Returns(http.StatusBadRequest, "error validating request", nil).
 		Returns(http.StatusInternalServerError, "something went wrong", nil))
 
 	// delete an item from the catalog
