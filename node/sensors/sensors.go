d0df24998274b5d106ab4c9ba15e1cbaf10a79c9
Mark deVilliers <markdevilliers@gmail.com>
2017-08-07 11:29:30 +0100

    comment Sensor message struct

--- node/sensors/sensors.go
+++ node/sensors/sensors.go
@@ -1,5 +1,6 @@
 package sensors
 
+// SensorMessage defines an internal message struct for dealing with sensor data
 type SensorMessage struct {
 	Data      map[string]interface{}
 	Schema    map[string]interface{}
